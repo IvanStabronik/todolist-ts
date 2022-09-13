7a59d4556a0318a7de9e50213b2668214676c605
Ivan Stabronik <engel.stabronik@gmail.com>
2022-09-13 13:29:47 +0300

    Updates

---
+++ precache-manifest.bee851af7c590d255495ad7b3214a663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bb3be60e3cdf3e3f324c17d07b5fe4",
+    "url": "//todolist-ts/index.html"
+  },
+  {
+    "revision": "a3ef3f81b8f19ea5f6fd",
+    "url": "//todolist-ts/static/css/main.8b8b6b6c.chunk.css"
+  },
+  {
+    "revision": "1b07c4b7380da76b66b6",
+    "url": "//todolist-ts/static/js/2.093bf16b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "//todolist-ts/static/js/2.093bf16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ef3f81b8f19ea5f6fd",
+    "url": "//todolist-ts/static/js/main.e7a29969.chunk.js"
+  },
+  {
+    "revision": "d1e0bf8cd22f9289fbd7",
+    "url": "//todolist-ts/static/js/runtime-main.ed381d1a.js"
+  }
+]);
